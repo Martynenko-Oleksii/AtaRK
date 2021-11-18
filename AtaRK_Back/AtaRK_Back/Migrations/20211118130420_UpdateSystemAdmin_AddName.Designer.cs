8480a6059c03d35a2a493c7164b84494ad5b8fa5
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-18 15:05:13 +0200

    Add name in SystemAdmin model

--- AtaRK_Back/AtaRK_Back/Migrations/20211118130420_UpdateSystemAdmin_AddName.Designer.cs
+++ AtaRK_Back/AtaRK_Back/Migrations/20211118130420_UpdateSystemAdmin_AddName.Designer.cs
@@ -4,14 +4,16 @@ using AtaRK.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AtaRK_Back.Migrations
 {
     [DbContext(typeof(ServerDbContext))]
-    partial class ServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211118130420_UpdateSystemAdmin_AddName")]
+    partial class UpdateSystemAdmin_AddName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AtaRK_Back/AtaRK_Back/Migrations/20211118130420_UpdateSystemAdmin_AddName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AtaRK_Back.Migrations
+{
+    public partial class UpdateSystemAdmin_AddName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "SystemAdmins",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "SystemAdmins");
+        }
+    }
+}
