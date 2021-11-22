dbef472fae28390137d692e84373651ed0ae3769
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-22 20:37:34 +0200

    Start admin page

---
+++ AtaRK_Back/AtaRK_Back/wwwroot/admin/js/login.js
@@ -0,0 +1,41 @@
+$( ".input" ).focusin(function() {
+    $( this ).find( "span" ).animate({"opacity":"0"}, 200);
+});
+  
+$( ".input" ).focusout(function() {
+    $( this ).find( "span" ).animate({"opacity":"1"}, 300);
+});
+  
+$(".login__form").submit(function(){
+    $(this).find(".submit i").removeAttr('class').addClass("fa fa-check").css({"color":"#fff"});
+    $(".submit").css({"background":"#2ecc71", "border-color":"#2ecc71"});
+    $(".feedback").show().animate({"opacity":"1", "bottom":"-80px"}, 400);
+    $("input").css({"border-color":"#2ecc71"});
+
+    const authDto = {
+        email: $("#email").val(), 
+        password: $("#password").val()
+    };
+
+    $.ajax({
+        type: "POST",
+        headers: {          
+            Accept: "application/json; charset=utf-8",         
+            "Content-Type": "application/json; charset=utf-8"   
+        },
+        url: "/api/sysadmins/login",
+        data: JSON.stringify(authDto),
+        success: function (data, textStatus, xhr) {
+            console.log(xhr.status);
+            localStorage.setItem("token", data["token"]);
+            $(".login").hide();
+        },
+        error: function (xhr, textStatus, errorThrown) {  
+            console.log(xhr.status);
+            console.log(textStatus);
+            console.log(errorThrown);
+        } 
+    });
+
+    return false;
+});
